1fd299aeec0ce1eb20d5a140dc372074431c1754
Jeroen Vinken <jeroen_vinken@hotmail.com>
2019-04-16 01:46:29 +0200

    Add sample assets + movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float baseRunSpeed = 20f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * baseRunSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;            
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Actually move character, never do this in update()
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false; // reset jump after jumping once
+    }
+}
