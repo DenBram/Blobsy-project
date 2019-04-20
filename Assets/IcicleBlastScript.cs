526d5067c15e344fa5d21c9c21373e080d172c98
Jeroen Vinken <jeroen_vinken@hotmail.com>
2019-04-20 23:47:35 +0200

    pew pew and more pew

---
+++ Assets/IcicleBlastScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleBlastScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject iciclePrefab;
+
+    private void Awake()
+    {
+        firePoint = GameObject.Find("/Player/IceFirePoint").transform;
+        iciclePrefab = Resources.Load<GameObject>("Prefabs/atk_icicle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireIcicle();
+        }
+    }
+
+    void FireIcicle()
+    {
+        Instantiate(iciclePrefab, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Assets/ProjectileScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public float baseDamage = 30f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+   void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        EnemyScript enemy = collision.GetComponent<EnemyScript>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(baseDamage);
+        }
+        Destroy(gameObject);
+    }
+}
