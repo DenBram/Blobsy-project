44f3aa565c31da69eb1f39db3cbd20ecd7e45194
Jeroen Vinken <jeroen_vinken@hotmail.com>
2019-04-21 02:19:20 +0200

    Love Spark laser & enemy movement

---
+++ Assets/LoveSparkScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoveSparkScript : MonoBehaviour
+{
+    public Transform firePoint;
+    public float baseDamage = 90f;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = GameObject.Find("/Player/IceFirePoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            StartCoroutine(LoveSpark());
+        }
+    }
+
+    IEnumerator LoveSpark()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+
+        if (hitInfo)
+        {
+            EnemyScript enemy =  hitInfo.transform.GetComponent<EnemyScript>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(baseDamage);
+            }
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+
+            //Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+        } else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right *100);
+        }
+
+        lineRenderer.enabled = true;
+
+        // wait some time
+        yield return new WaitForSeconds(0.08f);
+
+        lineRenderer.enabled = false;
+    }
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private float yVal;
+    private float increment = 0.01f;
+    //private Transform transform;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //transform = gameObject.GetComponent<SpriteRenderer>().transform;        
+        yVal = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Transform transform = gameObject.GetComponent<SpriteRenderer>().transform;
+        float newYPos = yVal + increment;
+        yVal = newYPos;
+        if (newYPos > 6 || newYPos < -6)
+        {
+            increment *= -1;
+        }
+        Vector3 newPosition = new Vector3(transform.position.x, newYPos, 0);
+        transform.position = newPosition;
+    }
+}
